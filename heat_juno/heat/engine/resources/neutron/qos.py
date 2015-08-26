MobileCloudNetworking/QOS_OS

commit 8f46fccbaaede850536149ac0ab1f997aae8c755

templates: demo: add missing network_id

heat_juno/heat/engine/resources/neutron/qos.py
@@ -26,7 +26,7 @@ class qos(neutron.NeutronResource):
     PROPERTIES = (
         TYPE, INGRESS_ID, EGRESS_ID, NET_ID, QOS_PARAMETERS
     ) = (
-        'type', 'ingress_id', 'egress_id', 'net_id', 'qos_parameters'
+        'type', 'ingress_id', 'egress_id', 'network_id', 'qos_parameters'
     )
 
     properties_schema = {
@@ -54,7 +54,7 @@ class qos(neutron.NeutronResource):
         NET_ID: properties.Schema(
             properties.Schema.STRING,
             _('Network identifier.'),
-            required=False,
+            required=True,
             update_allowed=True
         ),
         QOS_PARAMETERS: properties.Schema(
@@ -75,10 +75,9 @@ class qos(neutron.NeutronResource):
             'type': self.properties[self.TYPE],
             'ingress_id': self.properties[self.INGRESS_ID],
             'egress_id': self.properties[self.EGRESS_ID],
+            'net_id': self.properties[self.NET_ID]
             'qos_params': self.properties[self.QOS_PARAMETERS]
         }
-        if self.properties[self.NET_ID]:
-            info['net_id'] = self.properties[self.NET_ID]
 
         req = {'qos': info}
 

heat_templates/demo.yaml
@@ -61,12 +61,13 @@ resources:
       router_id: { get_resource: router }
       subnet_id: { get_resource: test_subnet_1 }
 
-  qos:
+  qos1:
     type: OS::Neutron::qos
     properties:
       type: unidir
       ingress_id: { get_resource: host1_port }
       egress_id: { get_resource: host2_port }
+      network_id: { get_resource: test_network_1 }
       qos_parameters: [ get_resource: qos_param1, get_resource: qos_param2 ]
 
   qos_param1:

commit 00b3daf0c6635ca68a861d9eef9175606b62e29f

heat: neutron: qos: fix bug introduced by 8f46fccbaaede850536149ac0ab1f997aae8c755

heat_juno/heat/engine/resources/neutron/qos.py
@@ -75,7 +75,7 @@ class qos(neutron.NeutronResource):
             'type': self.properties[self.TYPE],
             'ingress_id': self.properties[self.INGRESS_ID],
             'egress_id': self.properties[self.EGRESS_ID],
-            'net_id': self.properties[self.NET_ID]
+            'net_id': self.properties[self.NET_ID],
             'qos_params': self.properties[self.QOS_PARAMETERS]
         }
 